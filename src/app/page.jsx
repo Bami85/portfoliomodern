54e1002830294fb7ce94754809a546e88ffca5d1
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-09 21:30:38 +0200

    avatar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -49,8 +49,8 @@ const people = [
     imageUrl:
       'https://media.licdn.com/dms/image/D4E03AQEt8tW-sxhoFw/profile-displayphoto-shrink_400_400/0/1696878699766?e=1702512000&v=beta&t=bicOKT4E2ze5MyapKZqo0EFIuW_NzRt_1rX3ofz-wwE',
     bio: 'Team Manager Backup with approximately 5 year of practical experience. I m seeking to obtain a creative and challenging position that utilizes my current knowledge of web development and design. I have a positive attitude and I am always eager to learn new skills. I am comfortable working both independently and as part of a team',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+    githubUrlUrl: 'https://github.com/Bami85',
+    linkedinUrl: 'https://www.linkedin.com/in/ali-b-374813135/',
   },
   // More people...
 ]
@@ -69,8 +69,8 @@ function Avatar() {
                 <p className="mt-6 text-base leading-7 text-gray-600">{person.bio}</p>
                 <ul role="list" className="mt-6 flex gap-x-6">
                   <li>
-                    <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
-                      <span className="sr-only">Twitter</span>
+                    <a href={person.githubUrl} className="text-gray-400 hover:text-gray-500">
+                      <span className="sr-only">github</span>
                       <svg className="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                       </svg>

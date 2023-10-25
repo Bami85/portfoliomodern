0f32115cf57fd8e13ea27c206c05aa9332da88c1
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-25 11:57:16 +0200

    page target

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -40,11 +40,11 @@ export default function Page() {
                 >
                   Download CV
                 </a>
-                {/* <div className='flex items-center self-start mt-2'>
+                <div className='flex items-center self-start mt-2'>
                 <Link href ="/CV.pdf" target ={"_blank"} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark'>Show CV
                 
                 </Link>
-                </div>  */}
+                </div> 
 
                 </div>
             </FadeIn>
